0cc3033abf78e3607d98067e7126d20ea1b38042
André Araujo <andreolv.ar@gmail.com>
2023-01-21 17:33:02 -0300

    finalização

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -13,8 +13,7 @@ export function ProgressBar(props: ProgressBarProps) {
                 role="progressbar"
                 aria-label='Progresso de hábitos completados neste dia'
                 aria-valuenow={props.progress}
-                aria-
-                className='h-3 rounded-xl bg-violet-600'
+                className='h-3 rounded-xl bg-violet-600 transition-all'
                 style={progressStyle}
                 ></div>
         </div>
--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -1,3 +1,6 @@
+import dayjs from "dayjs";
+import { useEffect, useState } from "react";
+import { api } from "../lib/axios";
 import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning";
 import { HabitDay } from "./HabitDay";
 
@@ -7,7 +10,23 @@ const summaryDays = generateDatesFromYearBeginning();
 const minimumSummaryDaysSize = 18 * 7; // 18 weeks
 const ammountOfDaysToFill = minimumSummaryDaysSize - summaryDays.length;
 
+type Summary = Array<{
+    id: string;
+    date: string;
+    amount: number;
+    completed: number;
+}>;
+
 export function SummaryTable() {
+
+    const [summary, setSummary] = useState<Summary>([]);
+
+    useEffect(() => {
+        api.get('summary').then((response) => {
+            setSummary(response.data);
+        })
+    }, [])
+
     return (
         <div className="w-full flex ">
             <div className="grid grid-rows-7 grid-flow-row gap-3">
@@ -22,13 +41,19 @@ export function SummaryTable() {
             </div>
 
             <div className="grid grid-rows-7 grid-flow-col gap-3">
-                {summaryDays.map(day => {
+                {summary.length > 0 && summaryDays.map(date => {
+
+                    const dayInSummary = summary.find(day => {
+                        return dayjs(date).isSame(day.date, 'day');
+                    });
+
                     return (
                         
                         <HabitDay 
-                            key={day.toString()} 
-                            amount={5}
-                            completed={Math.random() * 5}
+                            key={date.toString()} 
+                            date={date}
+                            amount={dayInSummary?.amount}
+                            defaultCompleted={dayInSummary?.completed}
                         />
 
                     );
